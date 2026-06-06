f3f694f0677a6545aded2b765d072b6d92fcd215
Shavini Dilshara Joseph <shavinijoseph2004@gmail.com>
2026-06-06 15:27:06 +1000

    Update OnlineBookstoreApp.java

--- src/OnlineBookstoreApp.java
+++ src/OnlineBookstoreApp.java
@@ -1,12 +1,10 @@
-import javax.swing.SwingUtilities;
-
 public class OnlineBookstoreApp {
     public static void main(String[] args) {
         BookstoreStorage storage = new BookstoreStorage();
         BookstoreService service = new BookstoreService(storage);
-        SwingUtilities.invokeLater(() -> {
-            BookstoreAppUI ui = new BookstoreAppUI(service);
-            ui.setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            BookstoreAppUI app = new BookstoreAppUI(service);
+            app.setVisible(true);
         });
     }
 }
