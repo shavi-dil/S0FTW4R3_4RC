a4a075a10dcd295d3660f47306885edf652845bf
Shavini Dilshara Joseph <shavinijoseph2004@gmail.com>
2026-06-06 16:22:10 +1000

    Create Admin.java

---
+++ src/Admin.java
@@ -0,0 +1,8 @@
+/**
+ * Administrator user account for Ink and Lantern Books.
+ */
+public class Admin extends User {
+    public Admin(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
